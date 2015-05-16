459ca1812663224ea1f0531569cadee375b9101d
ichy <ichylinux@gmail.com>
2015-05-16 16:21:17 +0900

    bug fix

--- lib/closer.rb
+++ lib/closer.rb
@@ -1,13 +1,14 @@
 require "closer/version"
 
-if defined?(Rails)
-  require 'closer/rails/engine'
-  require 'closer/rails/railtie'
-end
-
 module Closer
 
   def self.config
     @@config ||= Closer::Config.new
   end
+
+end
+
+if defined?(Rails)
+  require 'closer/rails/engine'
+  require 'closer/rails/railtie'
 end
--- lib/closer/version.rb
+++ lib/closer/version.rb
@@ -1,3 +1,3 @@
 module Closer
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
