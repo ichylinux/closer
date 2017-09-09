bc2aa2a19b0c60199b307951af21e93350dafbf6
ichy <ichylinux@gmail.com>
2017-09-09 14:40:03 +0900

    cucumber patch

--- lib/closer/cucumber_patch.rb
+++ lib/closer/cucumber_patch.rb
@@ -1,5 +1,6 @@
 require 'cucumber'
 require 'cucumber/core/ast/step'
+require 'cucumber/formatter/console'
 
 module Cucumber
   module Core
@@ -11,6 +12,24 @@ module Cucumber
         end
 
       end
+    end
+  end
+end
+
+module Cucumber
+  module Formatter
+    module Console
+
+      def print_failing_scenarios(failures, custom_profiles, given_source)
+        @io.puts format_string("Failing Scenarios:", :failed)
+        failures.each do |failure|
+          profiles_string = custom_profiles.empty? ? '' : (custom_profiles.map{|profile| "-p #{profile}" }).join(' ') + ' '
+          source = given_source ? format_string(" # " + failure.name, :comment) : ''
+          @io.puts format_string("cucumber #{profiles_string}" + failure.location.force_encoding('UTF-8'), :failed) + source
+        end
+        @io.puts
+      end
+
     end
   end
 end
